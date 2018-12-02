93d3d12b4f0781f522da7bf049d224263c260fb2
Gaston11 <gaston@Debian9.gastonCasa>
2018-12-02 17:44:55 -0300

    alumno falta terminar

--- TPSeminario/grails-app/domain/tpseminario/Alumno.groovy
+++ TPSeminario/grails-app/domain/tpseminario/Alumno.groovy
@@ -1,12 +1,36 @@
 package tpseminario
+import componentes.*
 
 class Alumno {
 
     Date fechaNacimiento
     String nombre
     String telefono
-    int edad   
+    int edad
+    Reputacion reputacion   
 
     static constraints = {
+    	nombre black: false, nullable: false
+    	telefono black: false, nullable: false
+    	edad black: false, nullable: false, min: 0, max: 100
+    	fechaNacimiento black: false, nullable: false
     }
+
+    Alumno(){
+    	reputacion = new Reputacion()
+    }
+
+    void calificar(int calificacion){
+    	reputacion.calificar(calificacion)
+    }
+
+    void crearDeporte(){
+    	//hacer
+    }
+
+    void calificarDeporte(){
+    	//hacer
+    }
+
+    
 }
