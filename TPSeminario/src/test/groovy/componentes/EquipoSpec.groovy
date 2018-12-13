66fb119341bbfe099cf5b1c1a04b30af68d2194a
Gaston11 <gaston@Debian9.gastonCasa>
2018-12-13 20:35:17 -0300

    actualizaciones

---
+++ TPSeminario/src/test/groovy/componentes/EquipoSpec.groovy
@@ -0,0 +1,18 @@
+package componentes
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class EquipoSpec extends Specification implements DomainUnitTest<Equipo> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    //void "test something"() {
+      //  expect:"fix me"
+        //    true == false
+    //}
+}
---
+++ TPSeminario/src/test/groovy/componentes/PartidaSpec.groovy
@@ -0,0 +1,18 @@
+package componentes
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class PartidaSpec extends Specification implements DomainUnitTest<Partida> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
