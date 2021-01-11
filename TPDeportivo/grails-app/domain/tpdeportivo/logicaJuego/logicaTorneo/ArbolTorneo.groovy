fe5ad65e5d00524273da1b639f12bccacb5c3a2e
Amurrio <amurriog@hotmail.com>
2021-01-11 11:12:54 -0300

    agrego clase ArbolTorneo

---
+++ TPDeportivo/grails-app/domain/tpdeportivo/logicaJuego/logicaTorneo/ArbolTorneo.groovy
@@ -0,0 +1,38 @@
+package tpdeportivo.logicaJuego.logicaTorneo
+import tpdeportivo.logicaJuego.*
+
+class ArbolTorneo {
+
+    Nodo raiz = null
+
+    static constraints = {
+    }
+
+    Nodo getRaiz() {
+        return raiz;
+    }
+ 
+    void setRaiz(Partida partida) {
+        Nodo nodo = new Nodo(partida) 
+        this.raiz = nodo;
+    }
+
+    void setRaiz(Nodo nodo) {
+        this.raiz = nodo;
+    }
+
+    void agregarNodo(Nodo nodo){
+        if(raiz == null){
+            this.setRaiz(nodo)
+        } 
+        else{
+            if(raiz.getHojaDerecha() == null){
+                raiz.setHojaDerecha(nodo)
+            }
+            else{
+                (raiz.setHojaIquierda(nodo))
+            }
+        }
+    }
+
+}
