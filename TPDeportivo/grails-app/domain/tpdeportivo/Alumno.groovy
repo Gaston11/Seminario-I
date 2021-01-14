4596ed2f6fe61f88bffa602cdb26f07a28601557
Amurrio <amurriog@hotmail.com>
2021-01-14 17:33:19 -0300

    agrego funcion crearEquipo en Alumno

--- TPDeportivo/grails-app/domain/tpdeportivo/Alumno.groovy
+++ TPDeportivo/grails-app/domain/tpdeportivo/Alumno.groovy
@@ -1,24 +1,32 @@
 package tpdeportivo
+import tpdeportivo.logicaJuego.*
 
 class Alumno {
 
     String nombre
     Date fechaNacimiento
     String telefono
+    String email
+    Equipo equipo
     
     static hasMany = [deportes: Deporte]
 
     static constraints = {
-    	nombre black: false, nullable: false
-    	fechaNacimiento black: false, nullable: false
+        nombre black: false, nullable: false
+        fechaNacimiento black: false, nullable: false
         telefono black: false, nullable: true
-    	
+        email blank:false, email:true 
     }
 
     @Override
     public String toString() {
         return this.nombre;
     }
+
+    public void crearEquipo(int maximo){
+        Equipo unEquipo = new Equipo(maximo)
+        this.equipo = unEquipo 
+    } 
 
     
 }
