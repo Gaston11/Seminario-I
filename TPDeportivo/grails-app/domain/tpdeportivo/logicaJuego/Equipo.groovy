9e60c0d840c666e088ef7ddd41df6d62b89d7d37
Amurrio <amurriog@hotmail.com>
2021-01-15 12:21:37 -0300

    agrego clase Equipo

---
+++ TPDeportivo/grails-app/domain/tpdeportivo/logicaJuego/Equipo.groovy
@@ -0,0 +1,39 @@
+package tpdeportivo.logicaJuego
+import tpdeportivo.Alumno
+
+enum Estado {
+    ABIERTO,CERRADO
+}
+
+class Equipo {
+    String nombreEquipo
+    List<Alumno> listaAlumnos
+    int maximoAlumnos
+    Estado estado = Estado.ABIERTO
+
+    static constraints = {
+        nombreEquipo black: false, nullable: false
+        listaAlumnos black: true, nullable: true,editable:false
+        maximoAlumnos black: false, max: 4 // Setear maximo ??
+        estado black: false, nullable: false
+    }
+
+    Equipo (int maximo){
+        maximoAlumnos = maximo
+    }
+
+    public void agregarAlumnno(Alumno unAlumno){
+        if (listaAlumnos.size() < maximoAlumnos)
+            listaAlumnos.add(unAlumno)
+            else estado = CERRADO
+    }
+
+    public void eliminarAlumno(Alumno unAlumno){
+        listaAlumnos.remove(unAlumno)
+    }
+
+    @Override
+    public String toString() {
+        return this.nombreEquipo;
+    }
+}
